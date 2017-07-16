40d8b53d2d488925def05802c73309f160b1f294
Rnso <rnso.pnr@gmail.com>
2017-07-16 14:19:10 +0400

    updated from local

---
+++ server.js
@@ -0,0 +1,19 @@
+import config from './config'
+import express from 'express'
+
+const server = express()
+
+
+server.get('/', (req, res) => {
+   console.log(req.headers)
+  var info ={}
+  info.ip_address = req.headers['x-forwarded-for'].split(',')[0]
+  info.language = req.headers['accept-language'].split(',')[1].split(';')[0]
+  info.operating_System = req.headers['user-agent'].split('(')[1].split(')')[0]
+  res.send(info)
+})
+
+
+server.listen(config.port, () => {
+    console.info('Express Listening on port:', config.port)
+})
