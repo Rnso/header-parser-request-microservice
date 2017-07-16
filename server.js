7e2203ffd0c1c853aad3e14b1fc311fa65d5f0a9
Rnso <rnso.pnr@gmail.com>
2017-07-16 14:36:37 +0400

    update server.js

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ server.get('/', (req, res) => {
    console.log(req.headers)
   var info ={}
   info.ip_address = req.headers['x-forwarded-for'].split(',')[0]
+  if(req.headers['accept-language'] == 'en-GB,en-US;q=0.8,en;q=0.6'){
   info.language = req.headers['accept-language'].split(',')[1].split(';')[0]
+  }
+  else{
+    info.language = req.headers['accept-language'].split(',')[0]
+  }
   info.operating_System = req.headers['user-agent'].split('(')[1].split(')')[0]
   res.send(info)
 })
